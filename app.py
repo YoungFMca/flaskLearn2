236ae01d7df11a56f95dfd0320fdac07deb694f9
BisterTian <1274852499@qq.com>
2025-09-24 17:53:13 +0800

    完成

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ import os
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from forms import LoginForm
-from forms import UploadForm,MultiUploadForm
+from forms import UploadForm,MultiUploadForm,RichTextForm,NewPostForm
+from forms import SigninForm,RegisterForm,SigninForm2,RegisterForm2
 from flask import request 
 
 from flask import request,session,flash,redirect,url_for
 from flask_wtf.csrf import validate_csrf
 from wtforms import ValidationError
+from flask_ckeditor import CKEditor
 
 #from werkzeug import secure_filename
 import uuid as uid
@@ -120,7 +122,78 @@ def multi_upload():
         return redirect(url_for('show_images'))
     return render_template('upload.html',form=form)
 
+@app.route('/ckeditor',methods=['GET','POST'])
+def ckeditor():
+    form=RichTextForm()
+    if form.validate_on_submit():
+        title=form.title.data
+        body=form.body.data
+        flash('Your post is published!')
+        return render_template('post.html',title=title,body=body)
+    return render_template('ckeditor.html',form=form)
 
+@app.route('/two_submits',methods=['GET','POST'])
+def two_submits():
+    form=NewPostForm()
+    if form.validate_on_submit():
+        title=form.title.data
+        body=form.body.data
+        #区分到底点击的是哪个按钮
+        if form.save.data:
+            flash('You click the save button.')
+        elif form.publish.data:
+            flash('You click the publish button.')
+        return render_template('post.html',title=form.title.data,body=form.body.data)
+    return render_template('2submits.html',form=form)
+
+@app.route('/multi-form',methods=['GET','POST'])
+def multi_form():
+    signin_form=SigninForm()
+    register_form=RegisterForm()
+
+    if signin_form.submit1.data and signin_form.validate():
+        username=signin_form.username.data
+        flash('%s,you just submit the Signin Form.' % username)
+        return redirect(url_for('index'))
+    if register_form.submit2.data and register_form.validate():
+        flash('%s,you just submit the Register Form.' %username)
+        return redirect(url_for(index))
+    
+    return render_template('2form.html',signin_form=signin_form,register_form=register_form)
+#
+@app.route('/multi-form-multi-view')
+def multi_form_multi_view():
+    signin_form=SigninForm2()
+    register_form=RegisterForm2()
+    return render_template('2form2view.html',signin_form=signin_form,
+                           register_form=register_form)
+
+@app.route('/handle-signin',methods=['POST'])
+def handle_signin():
+    signin_form=SigninForm2()
+    register_form=RegisterForm2()
+
+    if signin_form.validate_on_submit():
+        username=signin_form.username.data
+        flash('%s,you ust submit the signin Form.' %username)
+        return redirect(url_for('index'))
+    return render_template('2form2view.html',signin_form=signin_form,register_form=register_form)
+
+@app.route('/handle-register',methods=['POST'])
+def handle_register():
+    signin_form=SigninForm2()
+    register_form=RegisterForm2()
+
+    if register_form.validate_on_submit():
+        username=register_form.username.data
+        flash('%s,you just submit the Register Form.' %username)
+        return redirect(url_for('index'))
+    return render_template('2form2view.html',signin_form=signin_form,register_form=register_form)
+
+# def flash_errors(form):
+#     for field,errors in form.errors.items():
+#         for error in errors:
+#             flash('Error in the %s field - %s' %(getattr(form,field).label.text,error))
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload=True
@@ -128,10 +201,15 @@ if __name__ == '__main__':
     app.secret_key=os.getenv('SECRET_KEY','secret string')
     app.config['ALLOWED_EXTENSIONS']=['png','jpg','jpeg','gif']
     app.config['UPLOAD_PATH']=os.path.join(app.root_path,'uploads')#路径 uploads文件夹
+    # Flask-CKEditor config
+    app.config['CKEDITOR_SERVE_LOCAL'] = True
+    app.config['CKEDITOR_FILE_UPLOADER'] = 'ckeditor'
     app.secret_key=os.getenv('SECRET_KEY','secret string')#用来获取环境变量中的密钥 第二个参数是默认值
+    ckeditor = CKEditor(app)
+
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-@app.before_first_request
-def befor_first_request():
-    print("Starting the Flask app...")
-    print(app.url_map)
+# @app.before_first_request
+# def befor_first_request():
+#     print("Starting the Flask app...")
+#     print(app.url_map)
--- forms.py
+++ forms.py
@@ -1,7 +1,38 @@
 from wtforms import Form,StringField,PasswordField,BooleanField,SubmitField
-from wtforms.validators import DataRequired,Length
+from wtforms.validators import DataRequired,Length,Email
 from flask_wtf import FlaskForm
 from flask_ckeditor import CKEditorField #从flask_cheditor导入
+
+
+class SigninForm2(FlaskForm):
+    username=StringField('用户名',validators=[DataRequired(),Length(1,20)])
+    password=PasswordField('密码',validators=[DataRequired(),Length(8,128)])
+    remember=BooleanField('记住我')
+    submit=SubmitField('注册')
+#
+class RegisterForm2(FlaskForm):
+    username=StringField('Username',validators=[DataRequired(),Length(1,20)])
+    email=StringField('Email',validators=[DataRequired(),Email(),Length(1,253)])
+    password=PasswordField('Password',validators=[DataRequired(),Length(1,254)])
+    submit=SubmitField('Register')
+
+class SigninForm(FlaskForm):
+    username=StringField('用户名',validators=[DataRequired(),Length(1,20)])
+    password=PasswordField('密码',validators=[DataRequired(),Length(8,128)])
+    remember=BooleanField('记住我')
+    submit1=SubmitField('注册')
+#
+class RegisterForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired(),Length(1,20)])
+    email=StringField('Email',validators=[DataRequired(),Email(),Length(1,253)])
+    password=PasswordField('Password',validators=[DataRequired(),Length(1,254)])
+    submit2=SubmitField('Register')
+
+class NewPostForm(FlaskForm):
+    title=StringField('标题',validators=[DataRequired(),Length(1,50)])
+    body=CKEditorField('正文',validators=[DataRequired()])
+    save=SubmitField('Save') #提交按钮
+    publish=SubmitField('Publish') #发布按钮
 
 class RichTextForm(FlaskForm):#富文本编辑器窗口
     title=StringField('Title',validators=[DataRequired(),Length(1,50)])
